af91ff044ed6d8a3493d4c6509f1c805c91eadd6
Jake VanderPlas <jakevdp@gmail.com>
2016-03-02 16:53:46 -0800

    add PyFlann method to refactored distance

--- megaman/geometry/distance_new.py
+++ megaman/geometry/distance_new.py
@@ -1,7 +1,14 @@
 import numpy as np
 from sklearn import neighbors
+from scipy import sparse
 
 from .cyflann.index import Index as CyIndex
+
+try:
+    import pyflann as pyf
+    PYFLANN_LOADED = True
+except ImportError:
+    PYFLANN_LOADED = False
 
 # from six.py
 def with_metaclass(meta, *bases):
@@ -45,6 +52,7 @@ class Adjacency(with_metaclass(AdjacencyMeta)):
         if (radius is None) == (n_neighbors is None):
            raise ValueError("Must specify either radius or n_neighbors, "
                             "but not both.")
+
 
     def adjacency_graph(self, X):
         if self.n_neighbors is not None:
@@ -105,6 +113,64 @@ class CyFLANNAdjacency(Adjacency):
     def knn_adjacency(self, X):
         cyindex = self._get_built_index(X)
         return cyindex.knn_neighbors_graph(X, self.n_neighbors)
+
+
+class PyFLANNAdjacency(Adjacency):
+    name = 'pyflann'
+
+    def __init__(self, radius=None, n_neighbors=None, flann_index=None,
+                 algorithm='kmeans', target_precision=0.9):
+        if not PYFLANN_LOADED:
+            raise ValueError("pyflann must be installed "
+                             "to use method='pyflann'")
+        self.flann_index = flann_index
+        self.algorithm = algorithm
+        self.target_precision = target_precision
+        super(PyFLANNAdjacency, self).__init__(radius=radius,
+                                               n_neighbors=n_neighbors,
+                                               mode='distance')
+
+    def _get_built_index(self, X):
+        if self.flann_index is None:
+            pyindex = pyf.FLANN()
+        else:
+            pyindex = self.flann_index
+
+        flparams = pyindex.build_index(X, algorithm=self.algorithm,
+                                       target_precision=self.target_precision)
+        return pyindex
+
+    def radius_adjacency(self, X):
+        flindex = self._get_built_index(X)
+
+        n_samples, n_features = X.shape
+        X = np.require(X, requirements = ['A', 'C']) # required for FLANN
+
+        graph_i = []
+        graph_j = []
+        graph_data = []
+        for i in range(n_samples):
+            jj, dd = flindex.nn_radius(X[i], self.radius ** 2)
+            graph_data.append(dd)
+            graph_j.append(jj)
+            graph_i.append(i*np.ones(jj.shape, dtype=int))
+
+        graph_data = np.concatenate(graph_data)
+        graph_i = np.concatenate(graph_i)
+        graph_j = np.concatenate(graph_j)
+        graph = sparse.coo_matrix((graph_data, (graph_i, graph_j)),
+                                  shape=(n_samples, n_samples))
+        graph.data = np.sqrt(graph.data) # FLANN returns squared distance
+        return graph
+
+    def knn_adjacency(self, X):
+        n_samples = X.shape[0]
+        flindex = self._get_built_index(X)
+        A_ind, A_data = flindex.nn_index(X, self.n_neighbors)
+        A_indptr = np.arange(0, n_samples * self.n_neighbors + 1,
+                             self.n_neighbors)
+        return sparse.csr_matrix((A_data.ravel(), A_ind.ravel(), A_indptr),
+                                 shape=(n_samples, n_samples))
 
 
 def adjacency_graph(X, method, *args, **kwargs):
--- megaman/geometry/tests/test_distance_new.py
+++ megaman/geometry/tests/test_distance_new.py
@@ -1,21 +1,31 @@
+from nose import SkipTest
+
 import numpy as np
 from numpy.testing import assert_allclose
 from scipy.sparse import isspmatrix
 
-
 from megaman.geometry.distance_new import adjacency_graph, Adjacency
+
+try:
+    import pyflann as pyf
+    NO_PYFLANN = False
+except ImportError:
+    NO_PYFLANN = True
 
 
 def test_adjacency():
     X = np.random.rand(100, 3)
     Gtrue = {}
 
-    assert len(Adjacency.methods()) == 4
+    assert len(Adjacency.methods()) == 5
 
     exact_methods = [m for m in Adjacency.methods()
                      if not m.endswith('flann')]
 
     def check_kneighbors(n_neighbors, method, exact=True):
+        if method == 'pyflann' and NO_PYFLANN:
+            raise SkipTest("pyflann not installed")
+
         G = adjacency_graph(X, method=method,
                             n_neighbors=n_neighbors)
         assert isspmatrix(G)
@@ -24,6 +34,9 @@ def test_adjacency():
             assert_allclose(G.toarray(), Gtrue[n_neighbors].toarray())
 
     def check_radius(radius, method, exact=True):
+        if method == 'pyflann' and NO_PYFLANN:
+            raise SkipTest("pyflann not installed")
+
         G = adjacency_graph(X, method=method,
                             radius=radius)
         assert isspmatrix(G)
