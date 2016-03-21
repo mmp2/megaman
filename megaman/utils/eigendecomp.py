1f770f9a38a03cc16bad2bfc78b724dd54f3b382
James McQueen <jmcq89@gmail.com>
2016-03-21 14:34:31 -0700

    generalized eigen_solver kewyword passing.

--- megaman/utils/eigendecomp.py
+++ megaman/utils/eigendecomp.py
@@ -11,6 +11,7 @@ from .validation import check_array
 
 EIGEN_SOLVERS = ['auto', 'dense', 'arpack', 'lobpcg']
 BAD_EIGEN_SOLVERS = {}
+AMG_KWDS = ['strength', 'aggregate', 'smooth', 'max_levels', 'max_coarse']
 
 try:
     from pyamg import smoothed_aggregation_solver
@@ -23,7 +24,7 @@ except ImportError:
     install pyamg or use another method."""
 
 
-def check_eigen_solver(eigen_solver, size=None, nvec=None):
+def check_eigen_solver(eigen_solver, solver_kwds, size=None, nvec=None):
     """Check that the selected eigensolver is valid
 
     Parameters
@@ -53,17 +54,21 @@ def check_eigen_solver(eigen_solver, size=None, nvec=None):
             warnings.warn("lobpcg does not perform well with small matrices or "
                           "with large numbers of vectors. Switching to 'dense'")
             eigen_solver = 'dense'
+            solver_kwds = None
 
         elif eigen_solver == 'auto':
             if size > 200 and nvec < 10:
                 if PYAMG_LOADED:
                     eigen_solver = 'amg'
+                    solver_kwds = None
                 else:
                     eigen_solver = 'arpack'
+                    solver_kwds = None
             else:
                 eigen_solver = 'dense'
+                solver_kwds = None
 
-    return eigen_solver
+    return eigen_solver, solver_kwds
 
 
 def _is_symmetric(M, tol = 1e-8):
@@ -75,8 +80,8 @@ def _is_symmetric(M, tol = 1e-8):
 
 
 def eigen_decomposition(G, n_components=8, eigen_solver='auto',
-                        random_state=None, eigen_tol=0.0,
-                        drop_first=True, largest=True):
+                        random_state=None,
+                        drop_first=True, largest=True, solver_kwds=None):
     """
     Function to compute the eigendecomposition of a square matrix.
 
@@ -110,8 +115,7 @@ def eigen_decomposition(G, n_components=8, eigen_solver='auto',
         A pseudo random number generator used for the initialization of the
         lobpcg eigen vectors decomposition when eigen_solver == 'amg'.
         By default, arpack is used.
-    eigen_tol : float, optional, default=0.0
-        Stopping criterion for eigendecomposition when using arpack eigen_solver
+    solver_kwds : any additional keyword arguments to pass to the selected eigen_solver
 
     Returns
     -------
@@ -121,9 +125,9 @@ def eigen_decomposition(G, n_components=8, eigen_solver='auto',
     if drop_first:
         n_components = n_components + 1
 
-    eigen_solver = check_eigen_solver(eigen_solver,
-                                      size=n_nodes,
-                                      nvec=n_components)
+    eigen_solver, solver_kwds = check_eigen_solver(eigen_solver, solver_kwds,
+                                                   size=n_nodes,
+                                                   nvec=n_components)
     random_state = check_random_state(random_state)
 
     # Convert G to best type for eigendecomposition
@@ -145,27 +149,39 @@ def eigen_decomposition(G, n_components=8, eigen_solver='auto',
             else:
                 which = 'SM'
             lambdas, diffusion_map = eigsh(G, k=n_components, which=which,
-                                           tol=eigen_tol, v0=v0)
+                                           v0=v0,**(solver_kwds or {}))
         else:
             if largest:
                 which = 'LR'
             else:
                 which = 'SR'
-            lambdas, diffusion_map = eigs(G, k=n_components, which=which,tol=eigen_tol)
+            lambdas, diffusion_map = eigs(G, k=n_components, which=which,
+                                          **(solver_kwds or {}))
         lambdas = np.real(lambdas)
         diffusion_map = np.real(diffusion_map)
     elif eigen_solver == 'amg':
+        # separate amg & lobpcg keywords:
+        if solver_kwds is not None:
+            amg_kwds = {}
+            lobpcg_kwds = solver_kwds.copy()
+            for kwd in AMG_KWDS:
+                if kwd in solver_kwds.keys():
+                    amg_kwds[kwd] = solver_kwds[kwd]
+                    del lobpcg_kwds[kwd]
+        else:
+            amg_kwds = None
+            lobpcg_kwds = None
         if not is_symmetric:
             raise ValueError("lobpcg requires symmetric matrices.")
         if not sparse.issparse(G):
             warnings.warn("AMG works better for sparse matrices")
         # Use AMG to get a preconditioner and speed up the eigenvalue problem.
-        ml = smoothed_aggregation_solver(check_array(G, accept_sparse = ['csr']))
+        ml = smoothed_aggregation_solver(check_array(G, accept_sparse = ['csr']),**(amg_kwds or {}))
         M = ml.aspreconditioner()
         n_find = min(n_nodes, 5 + 2*n_components)
         X = random_state.rand(n_nodes, n_find)
         X[:, 0] = (G.diagonal()).ravel()
-        lambdas, diffusion_map = lobpcg(G, X, M=M, largest=largest)
+        lambdas, diffusion_map = lobpcg(G, X, M=M, largest=largest,**(lobpcg_kwds or {}))
         sort_order = np.argsort(lambdas)
         if largest:
             lambdas = lambdas[sort_order[::-1]]
@@ -180,7 +196,7 @@ def eigen_decomposition(G, n_components=8, eigen_solver='auto',
             raise ValueError("lobpcg requires symmetric matrices.")
         n_find = min(n_nodes, 5 + 2*n_components)
         X = random_state.rand(n_nodes, n_find)
-        lambdas, diffusion_map = lobpcg(G, X, largest=largest)
+        lambdas, diffusion_map = lobpcg(G, X, largest=largest,**(solver_kwds or {}))
         sort_order = np.argsort(lambdas)
         if largest:
             lambdas = lambdas[sort_order[::-1]]
@@ -194,9 +210,9 @@ def eigen_decomposition(G, n_components=8, eigen_solver='auto',
         if sparse.isspmatrix(G):
             G = G.todense()
         if is_symmetric:
-            lambdas, diffusion_map = eigh(G)
+            lambdas, diffusion_map = eigh(G,**(solver_kwds or {}))
         else:
-            lambdas, diffusion_map = eig(G)
+            lambdas, diffusion_map = eig(G,**(solver_kwds or {}))
         if largest:# eigh always returns eigenvalues in ascending order
             lambdas = lambdas[::-1] # reverse order the e-values
             diffusion_map = diffusion_map[:, ::-1] # reverse order the vectors
@@ -205,8 +221,8 @@ def eigen_decomposition(G, n_components=8, eigen_solver='auto',
     return (lambdas, diffusion_map)
 
 
-def null_space(M, k, k_skip=1, eigen_solver='arpack', tol=1E-6, max_iter=100,
-               random_state=None):
+def null_space(M, k, k_skip=1, eigen_solver='arpack',
+               random_state=None, solver_kwds=None):
     """
     Find the null space of a matrix M: eigenvectors associated with 0 eigenvalues
 
@@ -236,23 +252,19 @@ def null_space(M, k, k_skip=1, eigen_solver='arpack', tol=1E-6, max_iter=100,
         'amg' :
             AMG requires pyamg to be installed. It can be faster on very large,
             sparse problems, but may also lead to instabilities.
-    tol : float, optional
-        Tolerance for 'arpack' method.
-        Not used if eigen_solver=='dense'.
-    max_iter : maximum number of iterations for 'arpack' method
-        not used if eigen_solver=='dense'
     random_state: numpy.RandomState or int, optional
         The generator or seed used to determine the starting vector for arpack
         iterations.  Defaults to numpy.random.
+    solver_kwds : any additional keyword arguments to pass to the selected eigen_solver
 
     Returns
     -------
     null_space : estimated k vectors of the null space
     error : estimated error (sum of eigenvalues)
     """
-    eigen_solver = check_eigen_solver(eigen_solver,
-                                      size=M.shape[0],
-                                      nvec=k + k_skip)
+    eigen_solver, solver_kwds = check_eigen_solver(eigen_solver, solver_kwds,
+                                                   size=M.shape[0],
+                                                   nvec=k + k_skip)
     random_state = check_random_state(random_state)
 
     if eigen_solver == 'arpack':
@@ -260,8 +272,7 @@ def null_space(M, k, k_skip=1, eigen_solver='arpack', tol=1E-6, max_iter=100,
         v0 = random_state.uniform(-1, 1, M.shape[0])
         try:
             eigen_values, eigen_vectors = eigsh(M, k + k_skip, sigma=0.0,
-                                                tol=tol, maxiter=max_iter,
-                                                v0=v0)
+                                                v0=v0,**(solver_kwds or {}))
         except RuntimeError as msg:
             raise ValueError("Error in determining null-space with ARPACK. "
                              "Error message: '%s'. "
@@ -275,7 +286,8 @@ def null_space(M, k, k_skip=1, eigen_solver='arpack', tol=1E-6, max_iter=100,
     elif eigen_solver == 'dense':
         if hasattr(M, 'toarray'):
             M = M.toarray()
-        eigen_values, eigen_vectors = eigh(M, eigvals=(0, k+k_skip),overwrite_a=True)
+        eigen_values, eigen_vectors = eigh(M, eigvals=(0, k+k_skip),overwrite_a=True,
+                                           **(solver_kwds or {}))
         index = np.argsort(np.abs(eigen_values))
         eigen_vectors = eigen_vectors[:, index]
         eigen_values = eigen_values[index]
@@ -292,7 +304,8 @@ def null_space(M, k, k_skip=1, eigen_solver='arpack', tol=1E-6, max_iter=100,
             eigen_values, eigen_vectors = eigen_decomposition(M, n_components,
                                                               eigen_solver = eigen_solver,
                                                               drop_first = False,
-                                                              largest = False)
+                                                              largest = False,
+                                                              solver_kwds=solver_kwds)
             eigen_values = eigen_values -1
             index = np.argsort(np.abs(eigen_values))
             eigen_values = eigen_values[index]
@@ -305,7 +318,8 @@ def null_space(M, k, k_skip=1, eigen_solver='arpack', tol=1E-6, max_iter=100,
             eigen_values, eigen_vectors = eigen_decomposition(M, n_components,
                                                               eigen_solver = eigen_solver,
                                                               drop_first = False,
-                                                              largest = False)
+                                                              largest = False,
+                                                              solver_kwds=solver_kwds)
             eigen_values = eigen_values - 2
             index = np.argsort(np.abs(eigen_values))
             eigen_values = eigen_values[index]
