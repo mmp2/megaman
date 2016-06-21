f09bf0f0bbfe3469d95c705ce2fc0acf3ddcf9bc
James McQueen <jmcq89@gmail.com>
2016-06-21 13:31:34 -0700

    Adding Nystrom Extension utility

---
+++ megaman/utils/nystrom_extension.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 21 11:11:40 2016
+
+@author: wang1
+"""
+from __future__ import division 
+import numpy as np
+def nystrom_extension(C, e_vec, e_val):
+    """
+    Parameters
+    ----------
+    C: array-like, shape = (n, l)
+      Stacking the training and testing data where n
+      is the total number of data and l is the number of 
+      training data.
+    e_val: array, shape = (1,s)
+      If W equals to C[0:l, :], then e_val are the largest s
+      eig values of W
+    e_vec: array-like, shape = (l, s)
+      These are the corresponding eig vectors to e_val
+    
+    Returns
+    -------
+    eval_nystrom: array-like, shape = (1,s)
+      These are the estimated largest s eig values of the matrix where C is the 
+      first l columns.
+    evec_nystrom: arrau-like, shape = (n, s)
+      These are the corresponding eig vectors to eval_nystrom
+      
+    """
+    n,l = C.shape    
+    W = C[0:l, :]
+    eval_nystrom = (n/l)*e_val
+    eval_inv = e_val.copy()
+    e_nonzero = [i for i, e in enumerate(e_val) if e != 0] #np.nonzero(a)[0]
+    eval_inv[e_nonzero] = 1.0/e_val[e_nonzero]
+    
+    evec_nystrom = np.dot(C,e_vec)*eval_inv
+    evec_nystrom = np.sqrt(l/n)*evec_nystrom
+    return eval_nystrom,evec_nystrom
+    
+    
+    
+    
+    
+    
+    
+    
+    
